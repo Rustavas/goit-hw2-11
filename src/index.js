c4e228aad9177a1934fc2dacbfc82a9f9ad549d8
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2024-01-25 19:35:41 +0200

    fix start page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('first')
+const URL_API = 'https://pixabay.com/api/';
+const KEY_API = '35530318-c832a4dcd48fc070f5c50cd79';
+
+const searchForm = document.getElementById('search-form');
+
+
+searchForm.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { searchQuery } = evt.currentTarget.elements
+  const inputValue = searchQuery.value.trim();
+
+  async function getRequest(inputValue) {
+    const resp = await fetch(`${URL_API}/?key=${KEY_API}`, {
+      q: inputValue,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true'
+      }) if (!resp.ok) {
+          throw new Error()
+        }
+    return resp.json()
+  }
+
+
+
+
+
+
+  // const q = inputValue;
+  // const image_type = 'photo';
+  // const orientation = 'horizontal';
+  // const safesearch = 'true'
+// }
+
+
