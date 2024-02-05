050a273e304b96912e949dcb8b44fb85a70ae3ea
Rustavas <115837907+Rustavas@users.noreply.github.com>
2024-02-05 19:17:57 +0000

    Deploying to gh-pages from @ Rustavas/goit-hw2-11@a8aec4cf6dacc735aaeb165a24261a775077bdfe 🚀

--- index.c0f8c5d4.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("search-form").addEventListener("submit",(function(e){e.preventDefault();const{searchQuery:t}=e.currentTarget.elements;t.value.trim();return resp.json()}));
-//# sourceMappingURL=index.c0f8c5d4.js.map
